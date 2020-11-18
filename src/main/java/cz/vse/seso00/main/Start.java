cb2b2f4a53f069750a35a9b7a3218de5261e251d
Ondřej Šesták <seso00@vse.cz>
2020-11-18 01:20:32 +0100

    Přidána javafx a metoda start

--- src/main/java/cz/vse/seso00/main/Start.java
+++ src/main/java/cz/vse/seso00/main/Start.java
@@ -3,6 +3,10 @@ package cz.vse.seso00.main;
 import cz.vse.seso00.model.Hra;
 import cz.vse.seso00.model.IHra;
 import cz.vse.seso00.textUi.TextoveRozhrani;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.sql.SQLOutput;
 
 /*******************************************************************************
  * Třída  Start je hlavní třídou projektu,
@@ -11,7 +15,7 @@ import cz.vse.seso00.textUi.TextoveRozhrani;
  * @author    Jarmila Pavlíčková
  * @version   ZS 2016/2017
  */
-public class Start
+public class Start extends Application
 {
     /***************************************************************************
      * Metoda, prostřednictvím níž se spouští celá aplikace.
@@ -19,14 +23,21 @@ public class Start
      * @param args Parametry příkazového řádku
      */
     public static void main(String[] args) {
-        IHra hra = new Hra();
-        TextoveRozhrani ui = new TextoveRozhrani(hra);
-        if (args.length ==0) {
-            ui.hraj();
+//        IHra hra = new Hra();
+//        TextoveRozhrani ui = new TextoveRozhrani(hra);
+//        if (args.length ==0) {
+//            ui.hraj();
+//
+//            //java -jar Adventura.jar Ahoj
+//        }else {
+//            ui.hrajZeSouboru(args[0]);
+//        }
+        launch();
+    }
 
-            //java -jar Adventura.jar Ahoj
-        }else {
-            ui.hrajZeSouboru(args[0]);
-        }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("startuji");
+        primaryStage.show();
     }
 }
